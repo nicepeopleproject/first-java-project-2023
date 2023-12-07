899b27fa5989aa6a5b7453e553d014880e8ce713
Avaz <avaz.akmataliev@yandex.ru>
2023-12-07 11:38:20 +0300

    pract 3

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -2,12 +2,15 @@ package org.example;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        Matematik schoolMatematik = new Matematik(100, 2);
+        Matematik schoolMatematik = new Matematik(100, 15);
         schoolMatematik.name = "Иван";
-        schoolMatematik.surname = "?";
+        schoolMatematik.surname = "Иванов";
 
 
         System.out.println("Уровень образования математика: " + schoolMatematik.getEducationLevel());
         System.out.println(schoolMatematik.calculateSquareSurface(5));
+        System.out.println(schoolMatematik.calculateCircleSurface(50));
+        System.out.println(schoolMatematik.calculateBallSurface(4));
+        System.out.println(schoolMatematik.calculatePrismaSurface(20, 2));
     }
 }
