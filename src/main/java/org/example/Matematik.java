f9432a875d45e637f9fc7580d737bad2f27383dc
jopistok <andrewrakitin@gmail.com>
2023-12-07 11:33:38 +0300

    4 methods

--- src/main/java/org/example/Matematik.java
+++ src/main/java/org/example/Matematik.java
@@ -28,13 +28,18 @@ public class Matematik {
     }
 
     public double calculateSquareSurface(double side) throws Exception {
-        if (educationLevel >= 2) {
+
+        if (side <= 0) {
+            // нужно выбрасывать ошибку
+            System.out.println("Сторона квадрата не может быть отрицательной");
+            return -1;
+        }
+
+        if (educationLevel == 10) {
+            System.out.println("Не хочу это делать, давай что-то лучше");
+            return -Math.PI;
+        } else if (educationLevel >= 2) {
             System.out.println("Я начал работать.");
-            if (side <= 0) {
-                // нужно выбрасывать ошибку
-                System.out.println("Сторона квадрата не может быть отрицательной");
-                return -1;
-            }
             Thread.sleep(100_000 / speed);
             return side * side;
         } else if (educationLevel == 1) {
@@ -47,6 +52,78 @@ public class Matematik {
                 return side * side / 2;
             }
         } else {
+            System.out.println("Ничего не могу сделать.");
+            return -999_999_999;
+        }
+    }
+    public double calculateCircleSurface(double radius) throws Exception {
+        if (radius <= 0) {
+            System.out.println("Радиус круга не может быть отрицательным");
+        }
+        System.out.println("Работа начата");
+        Thread.sleep(100_000 / speed);
+
+
+        if (educationLevel == 10) {
+            System.out.println("Не хочу это делать, давай что-то лучше");
+            return -Math.PI;
+        }else if (educationLevel >=3){
+            return Math.PI * radius * radius;
+        } else if (educationLevel == 2) {
+            System.out.println("Я только начинаю, могу ошибтся в этом непростом для меня вычислении.");
+            if (Math.random() > 0.4) {
+                Thread.sleep(100_000 / speed);
+                return Math.PI * radius * radius;
+            } else {
+                Thread.sleep(100_000 / speed);
+                return Math.PI * radius * radius * 0.25;
+            }
+        } else if (educationLevel == 1) {
+            System.out.println("Я только начинаю, могу ошибтся в этом непростом для меня вычислении.");
+            if (Math.random() > 0.6) {
+                Thread.sleep(100_000 / speed);
+                return Math.PI * radius * radius;
+            } else {
+                Thread.sleep(100_000 / speed);
+                return Math.PI * radius * radius * Math.PI;
+            }
+        } else {
+            System.out.println("Ничего не могу сделать.");
+            return -999_999_999;
+        }
+    }
+
+    public double calculatePrismSurface(double topSideArea, double sideArea, int sides) throws Exception{
+        if ((topSideArea <= 0)&(topSideArea <= 0)&(sides < 3)){
+            System.out.println("Аргументы функции невалидны");
+            return -1;
+        }
+
+        Thread.sleep(100_000 / speed);
+        if (educationLevel == 10) {
+            System.out.println("Не хочу это делать, давай что-то лучше");
+            return -Math.PI;
+        }else if (educationLevel >= 4){
+            return 2*topSideArea + (sideArea*sides);
+        }else{
+            System.out.println("Ничего не могу сделать.");
+            return -999_999_999;
+        }
+    }
+
+    public double calculateSphereSurface(double radius) throws Exception{
+        if (radius <= 0){
+            System.out.println("Аргументы функции невалидны");
+            return -1;
+        }
+
+        Thread.sleep(100_000 / speed);
+        if (educationLevel == 10) {
+            System.out.println("Не хочу это делать, давай что-то лучше");
+            return -Math.PI;
+        }else if (educationLevel >=5){
+            return 4.0*Math.PI*Math.pow(radius, 3)/3;
+        }else{
             System.out.println("Ничего не могу сделать.");
             return -999_999_999;
         }
