cdc347e19d527750249c338ca4ab3d93888f783c
Alexander <j32lemutov@gmail.com>
2023-12-09 23:39:40 +0300

    Matematik:edited

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -2,12 +2,15 @@ package org.example;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        Matematik schoolMatematik = new Matematik(100, 2);
+        Matematik schoolMatematik = new Matematik(100, 5);
         schoolMatematik.name = "Иван";
         schoolMatematik.surname = "?";
 
 
         System.out.println("Уровень образования математика: " + schoolMatematik.getEducationLevel());
         System.out.println(schoolMatematik.calculateSquareSurface(5));
+        System.out.println(schoolMatematik.calculatePrismSurface(5, 3, 4));
+        System.out.println(schoolMatematik.calculateSphereSurface(5));
+        System.out.println(schoolMatematik.calculateCircleSurface(5));
     }
 }
