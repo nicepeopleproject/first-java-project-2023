7494dbc4167e5faa21b154e7201a09a0270b13e8
BOSSIC <BOSSIC507@gmail.com>
2023-12-10 14:20:03 +0300

    Morin commit

--- src/main/java/org/example/Matematik.java
+++ src/main/java/org/example/Matematik.java
@@ -27,28 +27,105 @@ public class Matematik {
         return educationLevel;
     }
 
-    public double calculateSquareSurface(double side) throws Exception {
-        if (educationLevel >= 2) {
-            System.out.println("Я начал работать.");
-            if (side <= 0) {
+
+    public double calculateCirclCircl(double rad1) throws Exception {
+        if (educationLevel == 10) {
+
+            System.out.println("Я не хочу это считать.");
+        } else if (educationLevel >= 3) {
+
+            if ((rad1 <= 0)) {
                 // нужно выбрасывать ошибку
-                System.out.println("Сторона квадрата не может быть отрицательной");
+                System.out.println("Сторона не может быть отрицательной");
                 return -1;
             }
             Thread.sleep(100_000 / speed);
-            return side * side;
-        } else if (educationLevel == 1) {
+            return (Math.PI * rad1 * rad1);
+
+        } else if (educationLevel == 2) {
             System.out.println("Я только начинаю, могу ошибтся в этом непростом для меня вычислении.");
-            if (Math.random() > 0.5) {
+            if ((rad1 <= 0)) {
+                // нужно выбрасывать ошибку
+                System.out.println("Сторона не может быть отрицательной");
+                return -1;
+            }
+            if (Math.random() > 0.4) {
                 Thread.sleep(200_000 / speed);
-                return side * side;
+                return (Math.PI * rad1 * rad1);
             } else {
                 Thread.sleep(200_000 / speed);
-                return side * side / 2;
+                return (Math.PI * rad1 * rad1) / 3;
+
+            }
+        } else if (educationLevel == 1) {
+            System.out.println("Я только начинаю, могу ошибтся в этом непростом для меня вычислении.");
+            if ((rad1 <= 0)) {
+                // нужно выбрасывать ошибку
+                System.out.println("Сторона не может быть отрицательной");
+                return -1;
+            }
+            if (Math.random() > 0.6) {
+                Thread.sleep(200_000 / speed);
+                return (Math.PI * rad1 * rad1);
+            } else {
+                Thread.sleep(200_000 / speed);
+                return (Math.PI * rad1 * rad1) / 4;
             }
         } else {
-            System.out.println("Ничего не могу сделать.");
+            System.out.println("Я это не могу...");
             return -999_999_999;
         }
+        return rad1;
+    }
+
+    public double calculatePrizma(double side1, double side2, double high1) throws Exception {
+        if (educationLevel == 10) {
+            System.out.println("Я не хочу работать.");
+        } else if (educationLevel >= 4) {
+
+            if ((side1 <= 0) || (side2 <= 0) || (high1 <= 0)) {
+                // нужно выбрасывать ошибку
+                System.out.println("Стороны  не могут быть отрицательной");
+                return -1;
+            }
+            Thread.sleep(100_000 / speed);
+            return ((side1 * side2) * 2 + ((side1 * 2 + side2 * 2) * high1));
+
+        } else if (educationLevel == 0) {
+            System.out.println("Я это не могу...");
+            return -999_999_999;
+        }
+    return side1;
+    }
+
+    public double calculateCirclSQ(double rad1) throws Exception {
+
+        if (educationLevel == 10) {
+
+            System.out.println("Я не хочу это считать.");
+        } else if (educationLevel >= 5) {
+
+            if ((rad1 <= 0)) {
+                // нужно выбрасывать ошибку
+                System.out.println("Сторона не может быть отрицательной");
+                return -1;
+            }
+            Thread.sleep(100_000 / speed);
+            return (4*Math.PI * rad1 * rad1);
+
+        } else if (educationLevel == 0) {
+            System.out.println("Я это не могу...");
+            return -999_999_999;
+        }
+    return rad1;
     }
 }
+
+
+
+
+
+
+
+
+
--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -2,12 +2,15 @@ package org.example;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        Matematik schoolMatematik = new Matematik(100, 2);
+        Matematik schoolMatematik = new Matematik(100, 0);
         schoolMatematik.name = "Иван";
         schoolMatematik.surname = "?";
 
 
         System.out.println("Уровень образования математика: " + schoolMatematik.getEducationLevel());
-        System.out.println(schoolMatematik.calculateSquareSurface(5));
+        System.out.println(schoolMatematik.calculateCirclSQ(5));
+        System.out.println(schoolMatematik.calculateCirclCircl(6));
+        System.out.println(schoolMatematik.calculatePrizma(5,4,7));
     }
+//Морин Никита ИУ1-11Б
 }
